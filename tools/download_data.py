0f90a0cfa597fa3e09b022d2edccde71871f58ba
Krystof Woldrich <woldrkry@fel.cvut.cz>
2021-05-11 10:11:08 +0000

    Add data download and data unpack scripts

---
+++ tools/download_data.py
@@ -0,0 +1,31 @@
+import gdown
+
+# https://drive.google.com/file/d/0B4MuvwFQvf44VlVtTkVpaWR3cXM
+url = "https://drive.google.com/u/1/uc?id=0B4MuvwFQvf44VlVtTkVpaWR3cXM"
+output = "el1_512_512_512.zip"
+gdown.download(url, output, quiet=False)
+
+# https://drive.google.com/file/d/0B4MuvwFQvf44MkhYUVAyaEY1ME0
+url = "https://drive.google.com/u/1/uc?id=0B4MuvwFQvf44MkhYUVAyaEY1ME0"
+output = "el2_512_512_512.zip"
+gdown.download(url, output, quiet=False)
+
+# https://drive.google.com/file/d/0B4MuvwFQvf44U3NWZ0VXaENQTzQ
+url = "https://drive.google.com/u/1/uc?id=0B4MuvwFQvf44U3NWZ0VXaENQTzQ"
+output = "el3_512_512_512.zip"
+gdown.download(url, output, quiet=False)
+
+# https://drive.google.com/file/d/0B4MuvwFQvf44cmhKYWZhSW8wUHc
+url = "https://drive.google.com/u/1/uc?id=0B4MuvwFQvf44cmhKYWZhSW8wUHc"
+output = "mag1_512_512_512.zip"
+gdown.download(url, output, quiet=False)
+
+# https://drive.google.com/file/d/0B4MuvwFQvf44cWo4bHZDejMybnc
+url = "https://drive.google.com/u/1/uc?id=0B4MuvwFQvf44cWo4bHZDejMybnc"
+output = "mag2_512_512_512.zip"
+gdown.download(url, output, quiet=False)
+
+# https://drive.google.com/file/d/0B4MuvwFQvf44azdJaXBqX09vdnM
+url = "https://drive.google.com/u/1/uc?id=0B4MuvwFQvf44azdJaXBqX09vdnM"
+output = "mag3_512_512_512.zip"
+gdown.download(url, output, quiet=False)
